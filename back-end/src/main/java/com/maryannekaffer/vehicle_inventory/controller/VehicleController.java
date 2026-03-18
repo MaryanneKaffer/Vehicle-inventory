8356901e209c4f06b34546d07c61dba3d5683f58
maryanne <kaffermaryanne@gmail.com>
2026-03-18 16:50:58 -0300

    data

--- back-end/src/main/java/com/maryannekaffer/vehicle_inventory/controller/VehicleController.java
+++ back-end/src/main/java/com/maryannekaffer/vehicle_inventory/controller/VehicleController.java
@@ -24,7 +24,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.maryannekaffer.vehicle_inventory.entity.Vehicle;
 import com.maryannekaffer.vehicle_inventory.repository.VehicleRepository;
 
-@CrossOrigin(origins = "")
+@CrossOrigin(origins = "https://vehicle-inventory.vercel.app")
 @RestController
 @RequestMapping("/vehicles")
 public class VehicleController {
