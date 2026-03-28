b87d30f14ce80c3e4fa5ec1532d7c1f54a1f302a
maryanne <kaffermaryanne@gmail.com>
2026-03-27 22:06:25 -0300

    fix

--- front-end/src/layouts/default.tsx
+++ front-end/src/layouts/default.tsx
@@ -7,7 +7,7 @@ export default function DefaultLayout({
 }) {
   return (
     <div className="relative flex flex-col h-screen px-4 xl:px-0" id="top">
-      <main className="flex-1 px-2 md:px-8 xl:px-22 pt-12 min-h-[100dvh] w-full">
+      <main className="flex-1 px-2 md:px-8 xl:px-22 pt-12 h-fit w-full">
         {children}
       </main>
       <footer className="w-full flex items-center justify-center py-3" id="end">
